fbe33cf2a3b0d1a2f328bdd55c92301ae36aed98
vee-el <veronicalennon@live.com>
2023-02-27 01:41:53 +0000

    shows.js second draft

--- routes/shows.js
+++ routes/shows.js
@@ -2,26 +2,78 @@ const express = require('express');
 const app = express();
 const usersRouter = require('./routes/users');
 const showsRouter = require('./routes/shows');
+const router = express.Router();
+const Show = require('../models/show');
+
 
 app.use('/users', usersRouter);
 app.use('/shows', showsRouter);
 
+// Gets all shows from the database
 router.get('/', async (req, res) => {
-    // Get all shows from the database
-});
+    try {
+      const shows = await Show.find();
+      res.json(shows);
+    } catch (err) {
+      res.status(500).send('Server Error');
+    }
+});  
 
+// Gets one show by id from the database
 router.get('/:id', async (req, res) => {
-    // Get one show by id from the database
+    try {
+        const show = await Show.findById(req.params.id);
+        if (!show) {
+          return res.status(404).json({ message: 'Show not found' });
+        }
+        res.json(show);
+    } catch (err) {
+        res.status(500).send('Server Error');
+    }    
 });
 
+// Gets all shows of a particular genre from the database
 router.get('/genre/:genre', async (req, res) => {
-    // Get all shows of a particular genre from the database
+    try {
+        const shows = await Show.find({ genre: req.params.genre });
+        res.json(shows);
+    } catch (err) {
+        res.status(500).send('Server Error');
+    }
 });
 
+// Updates the status of a show in the database
 router.put('/:id/status', async (req, res) => {
-    // Update the status of a show in the database
+    try {
+      const { id } = req.params;
+      const { status } = req.body;
+  
+      const updatedShow = await Show.findByIdAndUpdate(id, { status }, { new: true });
+  
+      if (!updatedShow) {
+        return res.status(404).send('Show not found');
+      }
+  
+      res.json(updatedShow);
+
+    } catch (err) {
+      res.status(500).send('Server Error');
+    }
 });
+
+// Deletes a show from the database
 
 router.delete('/:id', async (req, res) => {
-    // Delete a show from the database
-});
+    try {
+      const { id } = req.params;
+
+      await Show.deleteOne({ _id: id });  
+
+      res.status(200).send(`Show with ID ${id} has been deleted.`);
+      
+    } catch (err) {
+      res.status(500).send('Server Error');
+    }
+  });
+  
+module.exports = router;
